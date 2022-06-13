ecc419f79f9da8e0e997c569de6d3730ec1ee1a6
Dara-To <33760969+Dara-To@users.noreply.github.com>
2022-06-12 23:58:48 -0400

    2. Attributed String

--- Weather/Views/Main/HomeView.swift
+++ Weather/Views/Main/HomeView.swift
@@ -25,12 +25,12 @@ struct HomeView: View {
                 .padding(.top, 257)
             
             // MARK: Current Weather
-            VStack {
+            VStack(spacing: -10) {
                 Text("Montreal")
                     .font(.largeTitle)
                 
                 VStack {
-                    Text("19°" + "\n" + "Mostly Clear")
+                    Text(attributedString)
                     
                     Text("H:24°   L:18°")
                         .font(.title3.weight(.semibold))
@@ -40,6 +40,27 @@ struct HomeView: View {
             }
             .padding(.top, 51)
         }
+    }
+    
+    private var attributedString: AttributedString {
+        var string = AttributedString("19°" + "\n " + "Mostly Clear")
+        
+        if let temp = string.range(of: "19°") {
+            string[temp].font = .system(size: 96, weight: .thin)
+            string[temp].foregroundColor = .primary
+        }
+        
+        if let pipe = string.range(of: " | ") {
+            string[pipe].font = .title3.weight(.semibold)
+            string[pipe].foregroundColor = .secondary
+        }
+        
+        if let weather = string.range(of: "Mostly Clear") {
+            string[weather].font = .title3.weight(.semibold)
+            string[weather].foregroundColor = .secondary
+        }
+        
+        return string
     }
 }
 
