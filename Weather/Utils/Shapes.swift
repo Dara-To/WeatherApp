3d06fa99afc465b08c29d6fa2eb118f8a1238569
Dara-To <33760969+Dara-To@users.noreply.github.com>
2022-06-16 05:01:04 -0400

    4. Arc and Custom Shapes

---
+++ Weather/Utils/Shapes.swift
@@ -0,0 +1,45 @@
+//
+//  Shapes.swift
+//  Weather
+//
+//  Created by Dara To on 2022-06-13.
+//
+
+import SwiftUI
+
+struct Arc: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: rect.minX - 1, y: rect.minY))
+        path.addQuadCurve(to: CGPoint(x: rect.maxX + 1, y: rect.minY), control: CGPoint(x: rect.midX, y: rect.midY))
+        path.addLine(to: CGPoint(x: rect.maxX + 1, y: rect.maxY + 1))
+        path.addLine(to: CGPoint(x: rect.minX - 1, y: rect.maxY + 1))
+        path.closeSubpath()
+        
+        return path
+    }
+}
+
+struct Trapezoid: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        let width = rect.size.width
+        let height = rect.size.height
+        path.move(to: CGPoint(x: 0, y: 0.37965*height))
+        path.addCurve(to: CGPoint(x: 0.03312*width, y: 0.02995*height), control1: CGPoint(x: 0, y: 0.18083*height), control2: CGPoint(x: 0, y: 0.08142*height))
+        path.addCurve(to: CGPoint(x: 0.21492*width, y: 0.04559*height), control1: CGPoint(x: 0.06623*width, y: -0.02153*height), control2: CGPoint(x: 0.1158*width, y: 0.00085*height))
+        path.addLine(to: CGPoint(x: 0.9003*width, y: 0.35499*height))
+        path.addCurve(to: CGPoint(x: 0.98602*width, y: 0.42173*height), control1: CGPoint(x: 0.94813*width, y: 0.37658*height), control2: CGPoint(x: 0.97204*width, y: 0.38738*height))
+        path.addCurve(to: CGPoint(x: width, y: 0.59997*height), control1: CGPoint(x: width, y: 0.45609*height), control2: CGPoint(x: width, y: 0.50405*height))
+        path.addLine(to: CGPoint(x: width, y: 0.74857*height))
+        path.addCurve(to: CGPoint(x: 0.98116*width, y: 0.96318*height), control1: CGPoint(x: width, y: 0.8671*height), control2: CGPoint(x: width, y: 0.92636*height))
+        path.addCurve(to: CGPoint(x: 0.87135*width, y: height), control1: CGPoint(x: 0.96232*width, y: height), control2: CGPoint(x: 0.93199*width, y: height))
+        path.addLine(to: CGPoint(x: 0.12865*width, y: height))
+        path.addCurve(to: CGPoint(x: 0.01884*width, y: 0.96318*height), control1: CGPoint(x: 0.06801*width, y: height), control2: CGPoint(x: 0.03768*width, y: height))
+        path.addCurve(to: CGPoint(x: 0, y: 0.74857*height), control1: CGPoint(x: 0, y: 0.92636*height), control2: CGPoint(x: 0, y: 0.8671*height))
+        path.addLine(to: CGPoint(x: 0, y: 0.37965*height))
+        path.closeSubpath()
+        return path
+    }
+}
--- Weather/Views/Navigation/TabBar.swift
+++ Weather/Views/Navigation/TabBar.swift
@@ -12,6 +12,16 @@ struct TabBar: View {
     
     var body: some View {
         ZStack {
+            // MARK: Arc Shape
+            Arc()
+                .fill(Color.tabBarBackground)
+                .frame(height: 88)
+                .overlay {
+                    // MARK: Arc Border
+                    Arc()
+                        .stroke(Color.tabBarBorder, lineWidth: 0.5)
+                }
+            
             // MARK: Tab Items
             HStack {
                 // MARK: Expand Button
