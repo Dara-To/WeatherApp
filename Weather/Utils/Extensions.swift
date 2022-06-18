beadeb23e34fe210002a447cf89c5adaee6191fb
Dara-To <33760969+Dara-To@users.noreply.github.com>
2022-06-18 04:22:23 -0400

    7. Inner Shadow

--- Weather/Utils/Extensions.swift
+++ Weather/Utils/Extensions.swift
@@ -29,3 +29,18 @@ extension View {
             )
     }
 }
+
+extension View {
+    func innerShadow<S: Shape, SS: ShapeStyle>(shape: S, color: SS, lineWidth: CGFloat = 1, offsetX: CGFloat = 0, offsetY: CGFloat = 0, blur: CGFloat = 4, blendMode: BlendMode = .normal, opacity: Double = 1) -> some View {
+        return self
+            .overlay {
+                shape
+                    .stroke(color, lineWidth: lineWidth)
+                    .blendMode(blendMode)
+                    .offset(x: offsetX, y: offsetY)
+                    .blur(radius: blur)
+                    .mask(shape)
+                    .opacity(opacity)
+            }
+    }
+}
--- Weather/Views/Detail/ForecastView.swift
+++ Weather/Views/Detail/ForecastView.swift
@@ -15,6 +15,7 @@ struct ForecastView: View {
         .backgroundBlur(radius: 25, opaque: true)
         .background(Color.bottomSheetBackground)
         .clipShape(RoundedRectangle(cornerRadius: 44))
+        .innerShadow(shape: RoundedRectangle(cornerRadius: 44), color: Color.bottomSheetBorderMiddle, lineWidth: 1, offsetX: 0, offsetY: 1, blur: 0, blendMode: .overlay, opacity: 1)
         .overlay {
             // MARK: Bottom Sheet Separator
             Divider()
