dc0109acbf41e093c72af567e04d095002a6ffed
Dara-To <33760969+Dara-To@users.noreply.github.com>
2022-06-23 02:55:04 -0400

    12. Conditional Scroll View

--- Weather/Views/Detail/ForecastView.swift
+++ Weather/Views/Detail/ForecastView.swift
@@ -13,9 +13,32 @@ struct ForecastView: View {
     
     var body: some View {
         ScrollView {
-            VStack(spacing: 20) {
+            VStack(spacing: 0) {
                 // MARK: Segmented Control
                 SegmentedControl(selection: $selection)
+                
+                // MARK: Forecast Cards
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 12) {
+                        if selection == 0 {
+                            ForEach(Forecast.hourly) { forecast in
+                                ForecastCard(forecast: forecast, forecastPeriod: .hourly)
+                            }
+                            .transition(.offset(x: -430))
+                        } else {
+                            ForEach(Forecast.daily) { forecast in
+                                ForecastCard(forecast: forecast, forecastPeriod: .daily)
+                            }
+                            .transition(.offset(x: 430))
+                        }
+                    }
+                    .padding(.vertical, 20)
+                }
+                .padding(.horizontal, 20)
+                
+                // MARK: Forecast Widgets
+                Image("Forecast Widgets")
+                    .opacity(bottomSheetTranslationProrated)
             }
         }
         .backgroundBlur(radius: 25, opaque: true)
